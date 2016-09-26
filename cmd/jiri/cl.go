729288cb06c73e6a509c20b284c9f7ed93f31171
Ankur Mittal <anmittal@google.com>
2016-09-26 15:51:25 -0700

    TO-52: Decouple jiri runhooks from projects
    Change-Id: I8527ad679717792f8520170c46d412e82d417940

--- cmd/jiri/cl.go
+++ cmd/jiri/cl.go
@@ -1538,7 +1538,7 @@ func patchCL(jirix *jiri.X, arg string) (e error) {
 			ref = c.Reference()
 		}
 
-		projects, _, err := project.LoadManifest(jirix)
+		projects, _, _, err := project.LoadManifest(jirix)
 		if err != nil {
 			return err
 		}
