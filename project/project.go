cb17566c3cc6da2516f18c5f26e66b21de383d76
Ankur Mittal <anmittal@google.com>
2016-10-25 13:10:03 -0700

    Support shared and reference cache option
    Supporting git cloning with --shared flag for bots. This method is much
    faster then --reference flag.
    
    Change-Id: Ic8732523c4bf18ea5b03e7b3877857ae1275c9fd

--- project/project.go
+++ project/project.go
@@ -1439,7 +1439,7 @@ func (ld *loader) load(jirix *jiri.X, root, file string, localManifest bool) err
 			if err := jirix.NewSeq().MkdirAll(path, 0755).Done(); err != nil {
 				return err
 			}
-			if err := gitutil.New(jirix.NewSeq()).Clone(p.Remote, path, ""); err != nil {
+			if err := gitutil.New(jirix.NewSeq()).Clone(p.Remote, path, "", false); err != nil {
 				return err
 			}
 			p.Revision = "HEAD"
@@ -2214,7 +2214,7 @@ func (op createOperation) Run(jirix *jiri.X, rebaseUntracked bool, snapshot bool
 		cache = ""
 	}
 
-	if err := gitutil.New(s).Clone(op.project.Remote, tmpDir, cache); err != nil {
+	if err := gitutil.New(s).Clone(op.project.Remote, tmpDir, cache, jirix.CacheShared); err != nil {
 		return err
 	}
 	if err := writeMetadata(jirix, op.project, tmpDir); err != nil {
