07d71ed91fda78eee1cb1f3b89eb50c0e059819b
Ankur Mittal <anmittal@google.com>
2017-05-17 18:27:08 -0700

    Add Progress msg support to logger
    Change-Id: I46d3c155b34765008cd48429ed54d960bcbe0a00

--- log/logger.go
+++ log/logger.go
@@ -5,11 +5,12 @@
 package log
 
 import (
+	"container/list"
 	"fmt"
-	"io"
 	glog "log"
 	"os"
 	"sync"
+	"time"
 
 	"fuchsia.googlesource.com/jiri/color"
 )
@@ -26,12 +27,29 @@ import (
 // log.Debugf(....)
 // By default Error logger prints to os.Stderr and others print to os.Stdout.
 // Capture function can be used to temporarily capture the logs.
+
+type TaskData struct {
+	msg      string
+	progress int
+}
+
+type Task struct {
+	taskData *TaskData
+	e        *list.Element
+	l        *Logger
+}
+
 type Logger struct {
-	lock          *sync.Mutex
-	LoggerLevel   LogLevel
-	goLogger      *glog.Logger
-	goErrorLogger *glog.Logger
-	color         color.Color
+	lock                 *sync.Mutex
+	LoggerLevel          LogLevel
+	goLogger             *glog.Logger
+	goErrorLogger        *glog.Logger
+	color                color.Color
+	progressLines        int
+	progressWindowSize   uint
+	enableProgress       bool
+	progressUpdateNeeded bool
+	tasks                *list.List
 }
 
 type LogLevel int
@@ -44,64 +62,153 @@ const (
 	TraceLevel
 )
 
-func NewLogger(loggerLevel LogLevel, color color.Color) *Logger {
-	return &Logger{
-		LoggerLevel:   loggerLevel,
-		lock:          &sync.Mutex{},
-		goLogger:      glog.New(os.Stdout, "", 0),
-		goErrorLogger: glog.New(os.Stderr, "", 0),
-		color:         color,
+func NewLogger(loggerLevel LogLevel, color color.Color, enableProgress bool, progressWindowSize uint) *Logger {
+	term := os.Getenv("TERM")
+	switch term {
+	case "dumb", "":
+		enableProgress = false
 	}
-}
-
-// Capture arranges for the next log to go to supplied io.Writers.
-// This will be cleared and not used for any subsequent logs.
-// Specifying nil for a writer will result in using the default writer.
-// ioutil.Discard should be used to discard output.
-func (l Logger) Capture(stdout, stderr io.Writer) Logger {
-	if stdout != nil {
-		l.goLogger = glog.New(stdout, "", 0)
+	l := &Logger{
+		LoggerLevel:          loggerLevel,
+		lock:                 &sync.Mutex{},
+		goLogger:             glog.New(os.Stdout, "", 0),
+		goErrorLogger:        glog.New(os.Stderr, "", 0),
+		color:                color,
+		progressLines:        0,
+		progressWindowSize:   progressWindowSize,
+		enableProgress:       enableProgress,
+		progressUpdateNeeded: false,
+		tasks:                list.New(),
 	}
-	if stderr != nil {
-		l.goErrorLogger = glog.New(stderr, "", 0)
+	if enableProgress {
+		go func() {
+			for {
+				l.repaintProgressMsgs()
+				time.Sleep(time.Second / 30)
+			}
+		}()
 	}
 	return l
 }
 
-func (l Logger) log(prefix, format string, a ...interface{}) {
+func (l *Logger) AddTaskMsg(format string, a ...interface{}) Task {
+	if !l.enableProgress {
+		return Task{taskData: &TaskData{}}
+	}
+	t := &TaskData{
+		msg:      fmt.Sprintf(format, a...),
+		progress: 0,
+	}
 	l.lock.Lock()
 	defer l.lock.Unlock()
+	e := l.tasks.PushBack(t)
+	l.progressUpdateNeeded = true
+	return Task{
+		taskData: t,
+		e:        e,
+		l:        l,
+	}
+}
+
+func (t *Task) Done() {
+	if !t.l.enableProgress {
+		return
+	}
+	t.taskData.progress = 100
+	t.l.lock.Lock()
+	defer t.l.lock.Unlock()
+	t.l.progressUpdateNeeded = true
+}
+
+func (l *Logger) repaintProgressMsgs() {
+	if !l.enableProgress {
+		return
+	}
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	if !l.progressUpdateNeeded {
+		return
+	}
+	l.clearProgress(0)
+	e := l.tasks.Front()
+	for i := uint(0); i < l.progressWindowSize; i++ {
+		for e != nil {
+			if t, ok := e.Value.(*TaskData); ok {
+				if t.progress < 100 {
+					l.printProgressMsg(t.msg)
+					e = e.Next()
+					break
+				} else {
+					temp := e.Next()
+					l.tasks.Remove(e)
+					e = temp
+				}
+			} else {
+				panic("Control should not come here")
+				return
+			}
+		}
+	}
+	l.progressUpdateNeeded = false
+}
+
+// This is thread unsafe
+func (l *Logger) printProgressMsg(msg string) {
+	str := fmt.Sprintf("%s: %s\n", l.color.Green("PROGRESS"), msg)
+	fmt.Printf(str)
+	l.progressLines++
+}
+
+// This is thread unsafe
+func (l *Logger) clearProgress(t time.Duration) {
+	if !l.enableProgress || l.progressLines == 0 {
+		return
+	}
+	buf := ""
+	for i := 0; i < l.progressLines; i++ {
+		buf = buf + "\033[1A\033[2K\r"
+	}
+	fmt.Printf(buf)
+	l.progressLines = 0
+	time.Sleep(t)
+}
+
+func (l *Logger) log(prefix, format string, a ...interface{}) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	l.clearProgress(time.Second / 30)
 	l.goLogger.Printf("%s%s", prefix, fmt.Sprintf(format, a...))
 }
 
-func (l Logger) Infof(format string, a ...interface{}) {
+func (l *Logger) Infof(format string, a ...interface{}) {
 	if l.LoggerLevel >= InfoLevel {
 		l.log("", format, a...)
 	}
 }
 
-func (l Logger) Debugf(format string, a ...interface{}) {
+func (l *Logger) Debugf(format string, a ...interface{}) {
 	if l.LoggerLevel >= DebugLevel {
 		l.log(l.color.Cyan("DEBUG: "), format, a...)
 	}
 }
 
-func (l Logger) Tracef(format string, a ...interface{}) {
+func (l *Logger) Tracef(format string, a ...interface{}) {
 	if l.LoggerLevel >= TraceLevel {
 		l.log(l.color.Blue("TRACE: "), format, a...)
 	}
 }
 
-func (l Logger) Warningf(format string, a ...interface{}) {
+func (l *Logger) Warningf(format string, a ...interface{}) {
 	if l.LoggerLevel >= WarningLevel {
 		l.log(l.color.Yellow("WARN: "), format, a...)
 	}
 }
 
-func (l Logger) Errorf(format string, a ...interface{}) {
+func (l *Logger) Errorf(format string, a ...interface{}) {
 	if l.LoggerLevel >= ErrorLevel {
 		l.lock.Lock()
 		defer l.lock.Unlock()
+		l.clearProgress(time.Second / 30)
 		l.goErrorLogger.Printf("%s%s", l.color.Red("ERROR: "), fmt.Sprintf(format, a...))
 	}
 }
