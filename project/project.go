5d7d6601c2586a4eb1bfaac2dd136f16f79ed454
Nathan Mulcahey <nmulcahey@google.com>
2018-10-28 10:46:19 -0700

    [cache] Set refspec at fetch time
    Because caches were previously created as a mirror, we have existing
    caches in the fleet with "refs/*" as their refspec and we need to avoid
    blindly calling fetch on those.
    
    Change-Id: I658994a8867be42a55d5466cfbd17766ba71e1e5

--- project/project.go
+++ project/project.go
@@ -22,6 +22,7 @@ import (
 	"fuchsia.googlesource.com/jiri"
 	"fuchsia.googlesource.com/jiri/gitutil"
 	"fuchsia.googlesource.com/jiri/log"
+	"fuchsia.googlesource.com/jiri/retry"
 )
 
 var (
@@ -1228,6 +1229,7 @@ func setRemoteHeadRevisions(jirix *jiri.X, remoteProjects Projects, localProject
 }
 
 func updateOrCreateCache(jirix *jiri.X, dir, remote, branch string, depth int) error {
+
 	if isPathDir(dir) {
 		if err := gitutil.New(jirix, gitutil.RootDirOpt(dir)).SetRemoteUrl("origin", remote); err != nil {
 			return err
@@ -1239,7 +1241,21 @@ func updateOrCreateCache(jirix *jiri.X, dir, remote, branch string, depth int) e
 		defer task.Done()
 		t := jirix.Logger.TrackTime(msg)
 		defer t.Done()
-		if err := fetch(jirix, dir, "origin", gitutil.PruneOpt(true)); err != nil {
+		// We need to explicitly specify the ref for fetch to update the bare
+		// repository.  Alternative would be to use
+		//
+		//   git config remote.origin.fetch "+refs/heads/*:refs/heads/*"
+		//
+		// when creating the cache in the other branch below.
+		refspec := "+refs/heads/*:refs/heads/*"
+		if depth > 0 {
+			// Shallow cache, fetch only manifest tracked remote branch
+			refspec = fmt.Sprintf("+refs/heads/%s:refs/heads/%s", branch, branch)
+		}
+		if err := retry.Function(jirix, func() error {
+			return gitutil.New(jirix, gitutil.RootDirOpt(dir)).FetchRefspec("origin", refspec, gitutil.PruneOpt(true))
+		}, fmt.Sprintf("Fetching for %s:%s", dir, refspec),
+			retry.AttemptsOpt(jirix.Attempts)); err != nil {
 			return err
 		}
 	} else {
@@ -1252,20 +1268,6 @@ func updateOrCreateCache(jirix *jiri.X, dir, remote, branch string, depth int) e
 		t := jirix.Logger.TrackTime(msg)
 		defer t.Done()
 		if err := gitutil.New(jirix).Clone(remote, dir, gitutil.BareOpt(true), gitutil.DepthOpt(depth)); err != nil {
-			return err
-		}
-		// We need to explicitly specify the ref for fetch to update the bare
-		// repository.  Alternative would be to use
-		//
-		//   git fetch --prune origin "+refs/heads/*:refs/heads/*"
-		//
-		// when updating the cache in the other branch above.
-		refspec := "+refs/heads/*:refs/heads/*"
-		if depth > 0 {
-			// Shallow cache, fetch only manifest tracked remote branch
-			refspec = fmt.Sprintf("+refs/heads/%s:refs/heads/%s", branch, branch)
-		}
-		if err := gitutil.New(jirix, gitutil.RootDirOpt(dir)).Config("remote.origin.fetch", refspec); err != nil {
 			return err
 		}
 	}
