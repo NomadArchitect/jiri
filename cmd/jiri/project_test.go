e329ce8e895c112702d5407e06124ed49e0ddc40
Nathan Mulcahey <nmulcahey@google.com>
2020-07-01 07:52:24 -0700

    [projects] Filter `jiri project` by attribute.
    This should prevent the command from attempting to act on repositories
    it does not have checked out on the local machine.
    
    Change-Id: I3f6fa85cb46f47780f6d899d5bab55867818e0bc

---
+++ cmd/jiri/project_test.go
@@ -0,0 +1,151 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"fuchsia.googlesource.com/jiri/jiritest"
+	"fuchsia.googlesource.com/jiri/project"
+)
+
+func TestProjectIgnoresByAttribute(t *testing.T) {
+	fake, cleanup := jiritest.NewFakeJiriRoot(t)
+	defer cleanup()
+
+	// Set up projects and packages with explict attributes
+	numProjects := 3
+	numOptionalProjects := 2
+	localProjects := []project.Project{}
+	totalProjects := numProjects + numOptionalProjects
+	// Project info returns the manifest project info as well.
+	numManifestProjects := 1
+
+	createRemoteProj := func(i int, attributes string) {
+		name := projectName(i)
+		path := fmt.Sprintf("path-%d", i)
+		if err := fake.CreateRemoteProject(name); err != nil {
+			t.Fatalf("failed to create remote project due to error: %v", err)
+		}
+		p := project.Project{
+			Name:       name,
+			Path:       filepath.Join(fake.X.Root, path),
+			Remote:     fake.Projects[name],
+			Attributes: attributes,
+		}
+		localProjects = append(localProjects, p)
+		if err := fake.AddProject(p); err != nil {
+			t.Fatalf("failed to add a project to manifest due to error: %v", err)
+		}
+	}
+
+	for i := 0; i < numProjects; i++ {
+		createRemoteProj(i, "")
+	}
+
+	for i := numProjects; i < totalProjects; i++ {
+		createRemoteProj(i, "optional")
+	}
+
+	// Create initial commit in each repo.
+	for _, remoteProjectDir := range fake.Projects {
+		writeReadme(t, fake.X, remoteProjectDir, "initial readme")
+	}
+
+	// Try default mode
+	fake.X.FetchingAttrs = ""
+	fake.UpdateUniverse(true)
+
+	file, err := ioutil.TempFile("", "tmp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	t.Log(file.Name())
+
+	stat, err := file.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Stat: %v", stat)
+
+	jsonOutputFlag = file.Name()
+	useRemoteProjects = true
+
+	runProject(fake.X, []string{})
+
+	var projectInfo []projectInfoOutput
+
+	file.Sync()
+
+	stat, err = file.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Stat: %v", stat)
+
+	file.Close()
+	bytes, err := ioutil.ReadFile(file.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(string(bytes))
+
+	err = json.Unmarshal(bytes, &projectInfo)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedProjects := numProjects + numManifestProjects
+
+	if len(projectInfo) != expectedProjects {
+		t.Errorf("Unexpected number of projects returned (%d, %d) (want, got)\n%v", expectedProjects, len(projectInfo), projectInfo)
+	}
+
+	// Try attributes
+	fake.X.FetchingAttrs = "optional"
+	fake.UpdateUniverse(true)
+
+	file2, err := ioutil.TempFile("", "tmp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file2.Name())
+
+	t.Log(file2.Name())
+
+	jsonOutputFlag = file2.Name()
+
+	runProject(fake.X, []string{})
+
+	file2.Sync()
+	file2.Close()
+	bytes, err = ioutil.ReadFile(file2.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(string(bytes))
+
+	err = json.Unmarshal(bytes, &projectInfo)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedProjects = totalProjects + numManifestProjects
+
+	if len(projectInfo) != expectedProjects {
+		t.Errorf("Unexpected number of projects returned (%d, %d) (want, got)\n%v", expectedProjects, len(projectInfo), projectInfo)
+	}
+}
