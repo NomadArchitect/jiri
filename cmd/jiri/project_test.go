62503455503490e40504c63d1f005129669d40b8
James Robinson <jamesr@google.com>
2020-12-22 16:34:27 -0800

    [license] Update license text and notices to new canonical form
    Bug: 66908
    Change-Id: Ie837ed89aaf5b864c75736e1781a883dffde5914

--- cmd/jiri/project_test.go
+++ cmd/jiri/project_test.go
@@ -1,6 +1,6 @@
-// Copyright 2020 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2020 The Fuchsia Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 package main
 
