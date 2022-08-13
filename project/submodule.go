dcfe98c75fad6d51fe37ef0d0f6d5df45c0349b9
Yuping Zhai <yupingz@google.com>
2022-08-12 20:15:08 -0700

    [jiri] Add snapshot submodule support
    Add submodule state check to determine local state. Add submodules in
    jiri projects format to jiri snapshot.
    
    Bug: b/237699717
    Change-Id: I85b90f85f40de593f6bdf683a74223aeb6c60c95

--- project/submodule.go
+++ project/submodule.go
@@ -4,6 +4,79 @@
 
 package project
 
+import (
+	"regexp"
+	"strings"
+
+	"go.fuchsia.dev/jiri"
+	"go.fuchsia.dev/jiri/gitutil"
+)
+
+type Submodule struct {
+	Name string
+	Path string
+	// Submodule SHA-1 prefix, could be "+", "-" or "U".
+	// "-" means submodule not initialized.
+	Prefix       string
+	Remote       string
+	Revision     string
+	Superproject string
+}
+
+type Submodules map[string]Submodule
+
+// containSubmodules checks if any of the projects contain submodules.
+func containSubmodules(jirix *jiri.X, projects Projects) bool {
+	for _, p := range projects {
+		if p.GitSubmodules {
+			if isSuperproject := isSuperproject(jirix, p); isSuperproject {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// getAllSubmodules return all submodules states
+func getAllSubmodules(jirix *jiri.X, projects Projects) []Submodules {
+	var allSubmodules []Submodules
+	for _, p := range projects {
+		if p.GitSubmodules {
+			if submodules := getSubmoduleStatus(jirix, p); submodules != nil {
+				allSubmodules = append(allSubmodules, submodules)
+			}
+		}
+	}
+	return allSubmodules
+}
+
+// getSubmoduleStatus returns submodule states in superproject.
+func getSubmoduleStatus(jirix *jiri.X, superproject Project) Submodules {
+	scm := gitutil.New(jirix, gitutil.RootDirOpt(superproject.Path))
+	submoduleStatus, _ := scm.SubmoduleStatus()
+	submodulesConfig := strings.Split(string(submoduleStatus), "\n")
+	submodules := make(Submodules)
+	for _, subm := range submodulesConfig {
+		var submodule Submodule
+		re := regexp.MustCompile(`[-+U]?([a-fA-F0-9]{40})\s(.*?)\s`)
+		pre := regexp.MustCompile(`([-+U])?`)
+		submConfig := re.FindAllString(subm, 1)[0]
+		submodule.Prefix = pre.FindAllString(subm, 1)[0]
+		submodule.Revision = strings.Split(submConfig, " ")[0]
+		submodule.Name = strings.Split(submConfig, " ")[1]
+		submodule.Remote, _ = scm.SubmoduleUrl(submodule.Name)
+		submodule.Superproject = superproject.Name
+		submodules[submodule.Name] = submodule
+		if submodule.Prefix == "+" {
+			jirix.Logger.Warningf("Submodule %s current checkout does not match the SHA-1 to the index of the containing repository.", submodule.Name)
+		}
+		if submodule.Prefix == "U" {
+			jirix.Logger.Warningf("Submodule %s has merge conflicts.", submodule.Name)
+		}
+	}
+	return submodules
+}
+
 // getSuperprojectStates returns the superprojects that have submodules enabled.
 func getSuperprojectStates(projects Projects) map[string]bool {
 	superprojectStates := make(map[string]bool)
@@ -13,6 +86,17 @@ func getSuperprojectStates(projects Projects) map[string]bool {
 		}
 	}
 	return superprojectStates
+}
+
+// isSuperproject checks if submodules exist under a project
+func isSuperproject(jirix *jiri.X, project Project) bool {
+	submodules := getSubmoduleStatus(jirix, project)
+	for _, subm := range submodules {
+		if subm.Prefix != "-" {
+			return true
+		}
+	}
+	return false
 }
 
 // removeSubmodulesFromProjects removes verified submodules from jiri projects.
