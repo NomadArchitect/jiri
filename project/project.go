3723c822970ad4a45766713dff14f7d911152f2d
Yuping <yupingz@google.com>
2023-03-07 23:30:50 -0800

    Add project flag to superproject when submodules are enabled
    Generate submodule flag to build/checkout.gni.
    
    Bug: b/272197216
    Change-Id: Ie4cc8d80d77357186936ea8a8b3704800ae7d225

--- project/project.go
+++ project/project.go
@@ -2574,6 +2574,14 @@ func updateProjects(jirix *jiri.X, localProjects, remoteProjects Projects, hooks
 	jirix.TimerPop()
 
 	jirix.TimerPush("jiri project flag files")
+	if jirix.EnableSubmodules {
+		for _, project := range remoteProjects {
+			if project.GitSubmodules {
+				project.Flag = "build/checkout.gni|submodules=true|submodules=false"
+			}
+		}
+	}
+
 	if err := WriteProjectFlags(jirix, remoteProjects); err != nil {
 		jirix.Logger.Errorf("failures in write jiri project flag files: %v", err)
 	}
